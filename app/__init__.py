67f5a84373f546ab857c4e1ca7d516e743765cd0
lyn27-hub <lianaanna968@gmail.com>
2026-01-16 21:10:13 +0700

    healthify dep1

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     # Inisialisasi database dan migrasi
     db.init_app(app)
     migrate.init_app(app, db)
-
+    
     # Register blueprint
     from app.routes.user_routes import user_bp
     app.register_blueprint(user_bp, url_prefix='/api/users')
